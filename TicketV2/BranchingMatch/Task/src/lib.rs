5107d9302bae6b7ec55900f00a04b7dc7407590d
Chakshu Jain <chakshujain1997@gmail.com>
2025-12-30 17:25:42 +0530

    "ticket v2 - branching - match" - done

--- TicketV2/BranchingMatch/Task/src/lib.rs
+++ TicketV2/BranchingMatch/Task/src/lib.rs
@@ -9,5 +9,11 @@ pub enum Shape {
 impl Shape {
     // TODO: Implement the `n_sides` method using a `match`.
     pub fn n_sides(&self) -> u8 {
-        /* TODO */    }
+        match self {
+            Shape::Circle => 0,
+            Shape::Square | Shape::Rectangle => 4,
+            Shape::Triangle => 3,
+            Shape::Pentagon => 5,
+        }
+    }
 }
