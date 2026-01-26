d9c5b3cc1d5a1bf1cd8f106f4949223bdf07c12b
Chakshu Jain <chakshujain1997@gmail.com>
2026-01-26 15:40:22 +0530

    "ticket management - slices" - done

--- TicketManagement/Slices/Task/src/lib.rs
+++ TicketManagement/Slices/Task/src/lib.rs
@@ -1,4 +1,6 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
 
-/* TODO */
+pub fn sum(slice: &[u32]) -> u32 {
+    slice.iter().sum()
+}
